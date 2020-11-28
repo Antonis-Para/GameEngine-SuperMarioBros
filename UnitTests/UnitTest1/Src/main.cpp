d9f339ee9a76fe3e907bf2ccabeaaf5e2cb45df3
Paragioudakis Antonis <antonismalia@yahoo.gr>
2020-11-28 17:40:48 +0200

    Convert absolut path to relative

--- UnitTests/UnitTest1/Src/main.cpp
+++ UnitTests/UnitTest1/Src/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 	app::TileMap m;
-	app::ReadTextMap(&m, "D:\\mraptakis\\Documents\\CSD\\Εξάμηνο 7ο\\ΗΥ-454\\Super_Mario\\UnitTests\\UnitTest1\\Media\\Overworld_GrassBiome\\map1_Kachelebene 1.csv");
+	app::ReadTextMap(&m, ".\\UnitTests\\UnitTest1\\Media\\Overworld_GrassBiome\\map1_Kachelebene 1.csv");
 
 	for (int i = 0; i < 42; ++i) {
 		for (int j = 0; j < 21; ++j)
