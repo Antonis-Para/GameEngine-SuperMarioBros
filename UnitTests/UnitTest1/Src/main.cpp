52755cbd89e0ea7556920fa6ac2c1d5d0d24f12e
csd4017 <csd4017@csd.uoc.gr>
2020-11-30 00:34:15 +0200

    made the event listeners

--- UnitTests/UnitTest1/Src/main.cpp
+++ UnitTests/UnitTest1/Src/main.cpp
@@ -8,32 +8,64 @@ extern app::Bitmap tiles;
 extern app::Bitmap dpyBuffer;
 
 int main() {
-    ALLEGRO_DISPLAY *display;
-    ALLEGRO_EVENT_QUEUE *queue;
+    ALLEGRO_DISPLAY* display;
+    ALLEGRO_EVENT_QUEUE* queue;
     //ALLEGRO_BITMAP *bitmap = NULL;
     //app::TileMap m;
+    bool scrollEnabled = false;
+    int mouse_x = 0, mouse_y = 0, prev_mouse_x = 0, prev_mouse_y = 0;
+    app::Rect viewWin = app::Rect{ 0, 0, 640, 480 };
+    app::Rect displayArea = app::Rect{ 0, 0, 640, 480 };
+    ALLEGRO_EVENT event;
 
     if (!al_init()) {
         std::cout << "ERROR: Could not init allegro\n";
         return -1;
     }
-    display = al_create_display(640, 480);
+    display = al_create_display(displayArea.w, displayArea.h);
 
     queue = al_create_event_queue();
     al_install_keyboard();
+    al_install_mouse();
+    al_register_event_source(queue, al_get_mouse_event_source());
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(display));
     al_init_image_addon();
-    tiles = app::BitmapLoad(".\\UnitTests\\UnitTest1\\Media\\Overworld_GrassBiome\\overworld_tileset_grass.png");
+    tiles = app::BitmapLoad(".\\hy-454-super-mario\\UnitTests\\UnitTest1\\Media\\Overworld_GrassBiome\\overworld_tileset_grass.png");
     assert(tiles != NULL);
 
-    dpyBuffer = app::BitmapCreate(640, 480);
+    dpyBuffer = app::BitmapCreate(displayArea.w, displayArea.h);
 
-    app::ReadTextMap(&map, ".\\UnitTests\\UnitTest1\\Media\\Overworld_GrassBiome\\map1_Kachelebene 1.csv");
+    app::ReadTextMap(&map, ".\\hy-454-super-mario\\UnitTests\\UnitTest1\\Media\\Overworld_GrassBiome\\map1_Kachelebene 1.csv");
     float x = 0;
     while (true) {
+        if (!al_is_event_queue_empty(queue)) {
+            al_wait_for_event(queue, &event);
+            if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
+                al_get_mouse_cursor_position(&mouse_x, &mouse_y);
+                if (scrollEnabled) {
+                    app::ScrollWithBoundsCheck(&viewWin, prev_mouse_x - mouse_x, prev_mouse_y - mouse_y); //Not ready yet but I got it :)
+                }
+                prev_mouse_x = mouse_x;
+                prev_mouse_y = mouse_y;
+            }
+            if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && event.mouse.button == 2) {
+                scrollEnabled = true;
+            }
+            else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP && event.mouse.button == 2) {
+                scrollEnabled = false;
+            }
+            else if (event.type == ALLEGRO_EVENT_KEY_DOWN && event.keyboard.keycode == ALLEGRO_KEY_HOME) {
+                viewWin.x = 0;
+                viewWin.y = 0;
+            }
+            else if (event.type == ALLEGRO_EVENT_KEY_DOWN && event.keyboard.keycode == ALLEGRO_KEY_END) {
+                viewWin.x = viewWin.w - displayArea.w; //Not ready yet but I got it :)
+                viewWin.y = viewWin.h - displayArea.h; //Not ready yet but I got it :)
+            }
+        }
         //al_clear_to_color(al_map_rgba_f(1, 1, 1, 1));
-        app::TileTerrainDisplay(&map, al_get_backbuffer(display), app::Rect{ 0, 0, 640, 480 }, app::Rect{ 0, 0, 640, 480 });
+        app::TileTerrainDisplay(&map, al_get_backbuffer(display), viewWin, displayArea);
         //al_flip_display();
 
         //al_draw_bitmap(dpyBuffer, 200, 0, 0);
