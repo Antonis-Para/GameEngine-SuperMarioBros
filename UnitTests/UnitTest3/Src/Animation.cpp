a644c2649704be60c89b6df54917a95d893d2a7e
csd4101 <csd4101@csd.uoc.gr>
2020-12-28 21:17:30 +0200

    Adding Animation-family calsses

--- UnitTests/UnitTest3/Src/Animation.cpp
+++ UnitTests/UnitTest3/Src/Animation.cpp
@@ -1,5 +1,180 @@
 #include "Animation.h"
 
+// Animation
+app::Animation::Animation(const std::string& _id) : id(_id) {}
+app::Animation::~Animation() {}
+
+const std::string& app::Animation::GetId(void) {
+	return id;
+}
+
+void app::Animation::SetId(const std::string& _id) {
+	id = _id;
+}
+
+// MovingAnimation
+app::MovingAnimation::MovingAnimation(const std::string& _id, unsigned _reps, int _dx, int _dy, unsigned _delay) :
+	Animation(_id), reps(_reps), dx(_dx), dy(_dy), delay(_delay) {}
+
+int app::MovingAnimation::GetDx(void) const {
+	return dx;
+}
+
+app::MovingAnimation::Me& app::MovingAnimation::SetDx(int v) {
+	dx = v;
+	return *this;
+}
+
+int app::MovingAnimation::GetDy(void) const {
+	return dy;
+}
+
+app::MovingAnimation::Me& app::MovingAnimation::SetDy(int v) {
+	dy = v;
+	return *this;
+}
+
+unsigned app::MovingAnimation::GetDelay(void) const {
+	return delay;
+}
+
+app::MovingAnimation::Me& app::MovingAnimation::SetDelay(unsigned v) {
+	delay = v;
+	return *this;
+}
+
+unsigned app::MovingAnimation::GetReps(void) const {
+	return reps;
+}
+
+app::MovingAnimation::Me& app::MovingAnimation::SetReps(unsigned n) {
+	reps = n;
+	return *this;
+}
+
+bool app::MovingAnimation::IsForever(void) const {
+	return !reps;
+}
+
+app::MovingAnimation::Me& app::MovingAnimation::SetForever(void) {
+	reps = 0;
+	return *this;
+}
+
+app::Animation* app::MovingAnimation::Clone(void) const {
+	return new MovingAnimation(id, reps, dx, dy, delay);
+}
+
+// FrameRangeAnimation
+app::FrameRangeAnimation::FrameRangeAnimation(const std::string& _id, unsigned s, unsigned e, unsigned r, int dx, int dy, int d):
+	start(s), end(e), MovingAnimation(id, r, dx, dy, d) {}
+
+unsigned app::FrameRangeAnimation::GetStartFrame(void) const {
+	return start;
+}
+
+app::FrameRangeAnimation::Me& app::FrameRangeAnimation::SetStartFrame(unsigned v) {
+	start = v;
+	return *this;
+}
+
+unsigned app::FrameRangeAnimation::GetEndFrame(void) const {
+	return end;
+}
+
+app::FrameRangeAnimation::Me& app::FrameRangeAnimation::SetEndFrame(unsigned v) {
+	end = v;
+	return *this;
+}
+
+app::Animation* app::FrameRangeAnimation::Clone(void) const {
+	return new FrameRangeAnimation(id, start, end, GetReps(), GetDx(), GetDy(), GetDelay());
+}
+
+// FrameListAnimation
+app::FrameListAnimation::FrameListAnimation(const std::string& _id, const Frames& _frames, unsigned r, int dx, int dy, unsigned d, bool c) :
+	frames(_frames), MovingAnimation(id, r, dx, dy, d) {}
+
+const app::FrameListAnimation::Frames& app::FrameListAnimation::GetFrames(void) const {
+	return frames;
+}
+
+void app::FrameListAnimation::SetFrames(const Frames& f) {
+	frames = f;
+}
+
+app::Animation* app::FrameListAnimation::Clone(void) const {
+	return new FrameListAnimation(id, frames, GetReps(), GetDx(), GetDy(), GetDelay(), true);
+}
+
+// MovingPathAnimation
+app::MovingPathAnimation::MovingPathAnimation(const std::string& _id, const Path& _path) :
+	path(_path), Animation(id) {}
+
+const app::MovingPathAnimation::Path& app::MovingPathAnimation::GetPath(void) const {
+	return path;
+}
+
+void app::MovingPathAnimation::SetPath(const Path& p) {
+	path = p;
+}
+
+app::Animation* app::MovingPathAnimation::Clone(void) const {
+	return new MovingPathAnimation(id, path);
+}
+
+// FlashAnimation
+app::FlashAnimation::FlashAnimation(const std::string& _id, unsigned n, unsigned show, unsigned hide) :
+	Animation(id), repetitions(n), hideDelay(hide), showDelay(show) {}
+
+app::FlashAnimation::Me& app::FlashAnimation::SetRepetitions(unsigned n) {
+	repetitions = n;
+	return *this;
+}
+
+unsigned app::FlashAnimation::GetRepetitions(void) const {
+	return repetitions;
+}
+
+app::FlashAnimation::Me& app::FlashAnimation::SetHideDeay(unsigned d) {
+	hideDelay = d;
+	return *this;
+}
+
+unsigned app::FlashAnimation::GetHideDeay(void) const {
+	return hideDelay;
+}
+
+app::FlashAnimation::Me& app::FlashAnimation::SetShowDeay(unsigned d) {
+	showDelay = d;
+	return *this;
+}
+
+unsigned app::FlashAnimation::GetShowDeay(void) const {
+	return showDelay;
+}
+
+app::Animation* app::FlashAnimation::Clone(void) const {
+	return new FlashAnimation(id, repetitions, hideDelay, showDelay);
+}
+
+// ScrollAnimation
+app::ScrollAnimation::ScrollAnimation(const std::string& _id, const Scroll& _scroll) :
+	Animation(_id), scroll(_scroll) {}
+
+const app::ScrollAnimation::Scroll& app::ScrollAnimation::GetScroll(void) const {
+	return scroll;
+}
+
+void app::ScrollAnimation::SetScroll(const Scroll& p) {
+	scroll = p;
+}
+
+app::Animation* app::ScrollAnimation::Clone(void) const {
+	return new ScrollAnimation(id, scroll);
+}
+
+// AnimationFilm
 app::AnimationFilm::AnimationFilm(const std::string& _id) : id(_id) {}
 app::AnimationFilm::AnimationFilm(Bitmap, const std::vector<Rect>&, const std::string&) {}
 
@@ -32,6 +207,7 @@ void app::AnimationFilm::Append(const Rect& r) {
 	boxes.push_back(r);
 }
 
+// AnimationFilmHolder
 auto app::AnimationFilmHolder::Get(void) -> const AnimationFilmHolder& {
 	return holder;
 }
@@ -65,6 +241,7 @@ auto app::AnimationFilmHolder::GetFilm(const std::string& id) -> const Animation
 	return i != films.end() ? i->second : nullptr;
 }
 
+// BitmapLoader
 app::BitmapLoader::BitmapLoader(void) {}
 app::BitmapLoader::~BitmapLoader() {
 	CleanUp();
@@ -75,7 +252,7 @@ Bitmap app::BitmapLoader::GetBitmap(const std::string& path) const {
 	return i != bitmaps.end() ? i->second : nullptr;
 }
 
-Bitmap app::BitmapLoader::BitmapLoad(const std::string& path) {
+Bitmap app::BitmapLoader::Load(const std::string& path) {
 	auto b = GetBitmap(path);
 	if (!b) {
 		bitmaps[path] = b = BitmapLoad(path);
--- UnitTests/UnitTest3/Src/Animation.h
+++ UnitTests/UnitTest3/Src/Animation.h
@@ -7,6 +7,132 @@
 #include "Defines.h"
 
 namespace app {
+	class Animation {
+	protected:
+		std::string id;
+	public:
+		Animation(const std::string&);
+
+		virtual ~Animation();
+		const std::string& GetId(void);
+		void SetId(const std::string&);
+		virtual Animation* Clone(void) const = 0;
+	};
+
+	class MovingAnimation: public Animation{
+	protected:
+		unsigned reps = 1; // 0=forever
+		int dx = 0, dy = 0;
+		unsigned delay = 0;
+	public:
+		MovingAnimation(const std::string& _id, unsigned _reps, int _dx, int _dy, unsigned _delay);
+
+		using Me= MovingAnimation;
+		int GetDx(void) const;
+		Me& SetDx(int v);
+		int GetDy(void) const;
+		Me& SetDy(int v);
+		unsigned GetDelay(void) const;
+		Me& SetDelay(unsigned v);
+		unsigned GetReps(void) const;
+		Me& SetReps(unsigned n);
+		bool IsForever(void) const;
+		Me& SetForever(void);
+		Animation* Clone(void) const override;
+	};
+
+	class FrameRangeAnimation: public MovingAnimation {
+	protected:
+		unsigned start = 0, end = 0;
+	public:
+		FrameRangeAnimation(const std::string& _id, unsigned s, unsigned e, unsigned r, int dx, int dy, int d);
+
+		using Me = FrameRangeAnimation;
+		unsigned GetStartFrame(void) const;
+		Me& SetStartFrame(unsigned v);
+		unsigned GetEndFrame(void) const;
+		Me& SetEndFrame(unsigned v);
+		Animation* Clone(void) const override;
+	};
+
+	class FrameListAnimation: public MovingAnimation {
+	public:
+		using Frames = std::vector<unsigned>;
+	protected:
+		Frames frames;
+	public:
+		FrameListAnimation(const std::string& _id, const Frames& _frames, unsigned r, int dx, int dy, unsigned d, bool c);
+
+		const Frames& GetFrames(void) const;
+		void SetFrames(const Frames& f);
+		Animation* Clone(void) const override;
+	};
+
+	struct PathEntry {
+		int dx = 0, dy = 0;
+		unsigned frame = 0;
+		unsigned delay = 0;
+		PathEntry(void) = default;
+		PathEntry(const PathEntry&) = default;
+	};
+	
+	class MovingPathAnimation: public Animation{
+	public:
+		using Path = std::vector<PathEntry>;
+	private:
+		Path path;
+	public:
+		const Path& GetPath(void) const {
+			return path;
+		}
+		void SetPath(const Path& p) {
+			path = p;
+		}
+		Animation* Clone(void) const override {
+			return new MovingPathAnimation(id, path);
+		}
+		MovingPathAnimation(const std::string& _id, const Path& _path): path(_path), Animation(id) {}
+	};
+
+	class FlashAnimation: public Animation {
+	private:
+		unsigned repetitions = 0;
+		unsigned hideDelay = 0;
+		unsigned showDelay = 0;
+	public:
+		using Me = FlashAnimation;
+
+		FlashAnimation(const std::string& _id, unsigned n, unsigned show, unsigned hide);
+
+		Me& SetRepetitions(unsigned n);
+		unsigned GetRepetitions(void) const;
+		Me& SetHideDeay(unsigned d);
+		unsigned GetHideDeay(void) const;
+		Me& SetShowDeay(unsigned d);
+		unsigned GetShowDeay(void) const;
+		Animation* Clone(void) const override;
+		
+	};
+
+	struct ScrollEntry {
+		int dx = 0;
+		int dy = 0;
+		unsigned delay = 0;
+	};
+
+	class ScrollAnimation: public Animation{
+	public:
+		using Scroll = std::vector<ScrollEntry>;
+	private:
+		Scroll scroll;
+	public:
+		ScrollAnimation(const std::string& _id, const Scroll& _scroll);
+
+		const Scroll& GetScroll(void) const;
+		void SetScroll(const Scroll& p);
+		Animation* Clone(void) const override;
+	};
+
 	class AnimationFilm {
 		std::vector<Rect> boxes;
 		Bitmap bitmap = nullptr;
@@ -14,6 +140,7 @@ namespace app {
 	public:
 		AnimationFilm(const std::string& _id) : id(_id) {}
 		AnimationFilm(Bitmap, const std::vector<Rect>&, const std::string&);
+
 		unsigned char GetTotalFrames(void) const;
 		Bitmap GetBitmap(void) const;
 		auto GetId(void) const -> const std::string&;
@@ -48,7 +175,7 @@ namespace app {
 	public:
 		BitmapLoader(void);
 		~BitmapLoader();
-		Bitmap BitmapLoad(const std::string& path);
+		Bitmap Load(const std::string& path);
 		void CleanUp(void);
 	};
 
