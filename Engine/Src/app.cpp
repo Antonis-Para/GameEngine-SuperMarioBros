853745a16ac73bdb35289b17355768822e08e655
csd4017 <csd4017@csd.uoc.gr>
2021-01-28 19:54:52 +0200

    fixing bg bug

--- Engine/Src/app.cpp
+++ Engine/Src/app.cpp
@@ -283,6 +283,7 @@ void recreateSprites(ALLEGRO_CONFIG* config, Game& game, bool checkpoint) {
 	mario = new Sprite(atoi(coordinates[0].c_str()), atoi(coordinates[1].c_str()), AnimationFilmHolder::GetInstance().GetFilm("Mario_small.stand_right"), "mario");
 	SpriteManager::GetSingleton().Add(mario);
 
+	underground_layer->SetViewWindow(Rect{ 0,0,action_layer->GetViewWindow().w,action_layer->GetViewWindow().h });
 	action_layer->SetViewWindow(Rect{ 0,0,action_layer->GetViewWindow().w,action_layer->GetViewWindow().h });
 
 	mario->SetMover([](const Rect& pos, int* dx, int* dy) {
