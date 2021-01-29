943f63b7204185f185f7771271d1077940b2e6e6
Paragioudakis Antonis <antonismalia@yahoo.gr>
2021-01-29 11:47:56 +0200

    πθση

--- Engine/Src/app.cpp
+++ Engine/Src/app.cpp
@@ -1036,7 +1036,7 @@ void app::MainApp::Initialise(void) {
 	assert(backgroundSong);
 	al_reserve_samples(3);
 	al_set_sample_instance_playmode(backgroundSong, ALLEGRO_PLAYMODE_LOOP);
-	al_set_sample_instance_gain(backgroundSong, .15f);
+	al_set_sample_instance_gain(backgroundSong, .10f);
 	al_attach_sample_instance_to_mixer(backgroundSong, al_get_default_mixer());
 	al_play_sample_instance(backgroundSong);
 
